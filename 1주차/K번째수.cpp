93fbfe8965ba178422655fe8fe48a2002ba678f0
Hyeongjin <33617083+BuildTheTruth@users.noreply.github.com>
2019-04-02 16:33:49 +0900

    1주차_프로그래머스
    출제자 : 배형진

--- 1주차/K번째수.cpp
+++ 1주차/K번째수.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 	vector<int> tmp, ans;
-	for (vector<int> comm : commands)
-	{
+	for (vector<int> comm : commands) {
 		tmp = array;
 		sort(tmp.begin() + comm[0] - 1, tmp.begin() + comm[1]);
 		ans.push_back(tmp[comm[0] + comm[2] - 2]);
